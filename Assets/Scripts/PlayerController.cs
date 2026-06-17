5de96bc7a5678a0c2a8024ffe6adae99ea914d40
Daniel Mercuri <Daniel.L.Mercuri@student.uts.edu.au>
2026-06-17 12:20:38 +1000

    commit 2
    map design + fpp camera movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    //public Camera camera;
+    //public InputActionAsset inputActions;
+
+    //private InputAction m_look;
+
+    //private void OnEnable()
+    //{
+    //    inputActions.FindActionMap("Player").Enable();
+    //}
+
+    //private void OnDisable()
+    //{
+    //    inputActions.FindActionMap("Player").Disable();
+    //}
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //m_look = InputSystem.actions.FindAction("Look");
+        Cursor.visible = true;
     }
 
     // Update is called once per frame
